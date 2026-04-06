807c2f8b614dff1e935d93e3c17142772d0f66c5
HtetO2Ko <hteto2ko.dev@gmail.com>
2026-04-07 00:27:27 +0630

    test: large payload

--- src/app.ts
+++ src/app.ts
@@ -37,6 +37,7 @@ const app = express();
 //// Security Secure
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
