e372b5c14cb5a5c127e4a16b0f107cfcd95b2ee1
HtetO2Ko <hteto2ko.dev@gmail.com>
2026-04-27 18:26:53 +0630

    V1.1.24

--- src/app.ts
+++ src/app.ts
@@ -108,7 +108,7 @@ app.use("/pdf", generatePDFController.router());
 app.get("/", (_req, res) => {
   res
     .status(200)
-    .json({ returncode: "200", message: "Lottery API V1.1.23 is working..." });
+    .json({ returncode: "200", message: "Lottery API V1.1.24 is working..." });
 });
 
 export default app;
